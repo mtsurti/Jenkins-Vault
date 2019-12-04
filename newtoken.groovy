0b132c35189cee7911027065ef9d8293cf6013ef
mtsurti <tahir.clinar@gmail.com>
2019-12-04 13:15:30 -0600

    Update newtoken.groovy

--- newtoken.groovy
+++ newtoken.groovy
@@ -1,18 +1,12 @@
 #! /usr/local/bin/groovy
 
-// script parameters
 def shuffleToken() {
   def userName = 'jenkins'
-  
-  /*def user = User.get(userName, false)
-  def apiTokenProperty = user.getProperty(ApiTokenProperty.class)
-  def result = apiTokenProperty.tokenStore.generateNewToken(tokenName)
-  user.save()*/
-  def tokenSeed = giveRandom() + userName + giveRandom()
+  def tokenSeed = nextRandomNum() + userName + nextRandomNum()
   def result = tokenSeed.bytes.encodeBase64().toString()
   println result
 }
-def giveRandom() {
+def nextRandomNum() {
   return Math.abs(new Random().nextInt() % ([1000000] - [500000])) + [500000]
 }
 return this
