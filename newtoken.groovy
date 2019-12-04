1b63d8f5b3566d6fe4f25d5c5e5eca5551dd2c65
mtsurti <tahir.clinar@gmail.com>
2019-12-04 14:00:23 -0600

    Update newtoken.groovy

--- newtoken.groovy
+++ newtoken.groovy
@@ -4,7 +4,7 @@ def shuffleToken() {
   def userName = 'jenkins'
   def tokenSeed = nextRandomNum() + userName + nextRandomNum()
   def result = tokenSeed.bytes.encodeBase64().toString()
-  println result
+  return result
 }
 def nextRandomNum() {
   return Math.abs(new Random().nextInt() % ([1000000] - [500000])) + [500000]
