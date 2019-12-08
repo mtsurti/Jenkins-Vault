eb721ab1c29feafb015c6d05e674f15ca5c0f073
mtsurti <tahir.clinar@gmail.com>
2019-12-08 01:03:08 -0600

    Update ParseConfig.groovy

--- ParseConfig.groovy
+++ ParseConfig.groovy
@@ -13,6 +13,7 @@ def updateAllConfigs(String token) {
               def configXMLFile = thisJob.getConfigFile()
               def file = configXMLFile.getFile()
               file.eachLine { line ->
+                    println "Just printing " + line.toLowerCase()
                     if (line.toLowerCase().contains("authToken")) {
                         println line
                     }
