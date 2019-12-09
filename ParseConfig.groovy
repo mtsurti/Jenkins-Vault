7a3ee02a213415736a6d65a7ff5f608ed38c5fff
mtsurti <tahir.clinar@gmail.com>
2019-12-09 11:44:31 -0600

    Update ParseConfig.groovy

--- ParseConfig.groovy
+++ ParseConfig.groovy
@@ -69,7 +69,7 @@ def updateAllConfigs(String token) {
               }*/
               //println "config file is " + file
               
-              sh "mv " + pwd() + "/config.xml " + "/Users/mohammad/.jenkins/jobs/token-rotator"
+              sh "mv " + pwd() + "/config.xml " + " /Users/mohammad/.jenkins/jobs/token-rotator"
               //sh "rm /Users/mohammad/.jenkins/jobs/token-rotator/config.xml"
               //sh "mv /Users/mohammad/.jenkins/jobs/token-rotator/tempconfig.xml /Users/mohammad/.jenkins/jobs/token-rotator/config.xml"
               
