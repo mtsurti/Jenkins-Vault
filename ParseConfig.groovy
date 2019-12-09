41513ff25a3bea712d55394bfcbc67bc91e84ccf
mtsurti <tahir.clinar@gmail.com>
2019-12-09 12:12:29 -0600

    Update ParseConfig.groovy

--- ParseConfig.groovy
+++ ParseConfig.groovy
@@ -74,9 +74,9 @@ def updateAllConfigs(String token) {
               //sh "rm /Users/mohammad/.jenkins/jobs/token-rotator/config.xml"
               //sh "mv /Users/mohammad/.jenkins/jobs/token-rotator/tempconfig.xml /Users/mohammad/.jenkins/jobs/token-rotator/config.xml"
               
-              thisJob.updateByXml(new StreamSource(newInputStream("/Users/mohammad/.jenkins/jobs/token-rotator/config.xml")))
-              thisJob.save()
-              thisJob.doReload() 
+              //thisJob.updateByXml(new StreamSource(newInputStream("/Users/mohammad/.jenkins/jobs/token-rotator/config.xml")))
+              //thisJob.save()
+              //thisJob.doReload() 
                        
       /*
             import jenkins.model.Jenkins;
