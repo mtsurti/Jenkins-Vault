1b62cd1ebac7dd7eb6f1bfdb2e81c227610d60b3
mtsurti <tahir.clinar@gmail.com>
2019-12-09 13:04:56 -0600

    Update ParseConfig.groovy

--- ParseConfig.groovy
+++ ParseConfig.groovy
@@ -7,7 +7,6 @@ def updateAllConfigs(String token) {
       PrintWriter output = new PrintWriter(pwd()+'/config.xml', 'utf-8')
       def prefix
       for (thisJob in hudson.model.Hudson.instance.items) {   
-          //def prefix = names.substring(0, names.indexOf('-'))
           prefix = thisJob.name.takeWhile { it != '-' }
           if (prefix.toLowerCase().contains("token")) {
               def configXMLFile = thisJob.getConfigFile()
@@ -31,7 +30,6 @@ def updateAllConfigs(String token) {
               }
               output.close()
        
-              println "mv " + pwd() + "/config.xml" + " /Users/mohammad/.jenkins/jobs/token-rotator"
               sh "mv " + pwd() + "/config.xml " + " /Users/mohammad/.jenkins/jobs/token-rotator"
               
               //thisJob.updateByXml(new InputStream(pwd()+"/config.xml"))
