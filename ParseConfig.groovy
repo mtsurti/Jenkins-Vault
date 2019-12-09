3c58a8fe0da29dce5afe5abb4a8d5d45ff5936d7
mtsurti <tahir.clinar@gmail.com>
2019-12-08 23:14:05 -0600

    Update ParseConfig.groovy

--- ParseConfig.groovy
+++ ParseConfig.groovy
@@ -93,7 +93,7 @@ def updateAllConfigs(String token) {
                   }
               //}*/
           }
-        }
+        //}
       }         
       /*
             import jenkins.model.Jenkins;
