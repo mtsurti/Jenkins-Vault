ca3e5f3129848e7672d74981676958306cc4d21b
mtsurti <tahir.clinar@gmail.com>
2019-12-10 11:48:31 -0600

    Update parseConfig.groovy

--- parseConfig.groovy
+++ parseConfig.groovy
@@ -4,14 +4,20 @@
 def updateAllConfigs(String token) {
       def hudson = hudson.model.Hudson.instance;
       PrintWriter output 
+      String jobname
       def prefix
-      for (aJob in Hudson.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob)){ //org.jenkinsci.plugins.workflow.job.WorkflowJob)*.fullName)
+      /*for (aJob in Hudson.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob)){ //org.jenkinsci.plugins.workflow.job.WorkflowJob)*.fullName)
             println aJob.fullName
+      }*/
+      Jenkins.instance.getAllItems(Job.class).findAll.each { job->
+        jobname = item.getUrl()
+        jobname = jobname.replaceAll('job/', '')
+        println jobname
       }
-      
       //for (thisJob in hudson.model.Hudson.instance.items) {   
       for (thisJob in Hudson.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob)){
           prefix = thisJob.fullName.takeWhile { it != '-' }
+          println prefix
           if (prefix.toLowerCase().contains("iaas")) {
               output = new PrintWriter(pwd()+'/config.xml', 'utf-8')
               def configXMLFile = thisJob.getConfigFile()
@@ -25,14 +31,14 @@ def updateAllConfigs(String token) {
                         output.println line
                     }
               }
-              output.close()
+              
               println "mv " + pwd() + "/config.xml " + " /Users/mohammad/.jenkins/jobs/" + thisJob.fullName            
               sh "mv " + pwd() + "/config.xml " + " /Users/mohammad/.jenkins/jobs/" + thisJob.fullName          
               //thisJob.updateByXml(new InputStream(pwd()+"/config.xml"))
               thisJob.save()
-              thisJob.doReload() 
-                       
+              thisJob.doReload()        
            }
+        output.close()
         }
       }                
 return this
