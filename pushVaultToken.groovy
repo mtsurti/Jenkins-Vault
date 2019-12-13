a855a31cb9e18a72d2acedb0a9de4fdd58f943ab
mtsurti <tahir.clinar@gmail.com>
2019-12-13 14:09:56 -0600

    Update pushVaultToken.groovy

--- pushVaultToken.groovy
+++ pushVaultToken.groovy
@@ -50,7 +50,7 @@ import hudson.util.Secret
  
     // optional configuration, if you do not provide this the next higher configuration
     // (e.g. folder or global) will be used
-    def configuration = [vaultUrl: "https://" + hostname + ":8200",
+    def configuration = [vaultUrl: "http://" + hostname + ":8200",
                          vaultCredentialId: 'vault-token-id']
     // inside this block your credentials will be available as env variables
     withVault([configuration: configuration, vaultSecrets: secrets]) {
