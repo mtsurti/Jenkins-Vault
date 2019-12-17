246798a7bebad7bcd4b87623570e5059a0ad22bf
mtsurti <tahir.clinar@gmail.com>
2019-12-17 14:31:36 -0600

    Update pushVaultToken.groovy

--- pushVaultToken.groovy
+++ pushVaultToken.groovy
@@ -41,10 +41,10 @@ import hudson.util.Secret
     if (authToken != null && authToken.length() > 0 &&
        vaultToken != null && vaultToken.length() > 0 &&
        vaultAddress != null && vaultAddress.length() > 0) {
-        println "vault write -address="+ vaultAddress + "v1/kv/my-secret value=" + vaultToken
-        sh "vault write -address="+ vaultAddress + "v1/kv/my-secret value=" + vaultToken
+        //println "vault write -address="+ vaultAddress + "v1/kv/my-secret value=" + vaultToken
+        //sh "vault write -address="+ vaultAddress + "v1/kv/my-secret value=" + vaultToken
 
-        //println "curl --header \"X-Vault-Token: " + vaultToken + "\" --request PUT -d {\"my-value\",\"" + vaultToken + "\"} " + vaultAddress + "/v1/kv/my-secret | jq"
+        println "curl --header \"X-Vault-Token: " + vaultToken + "\" --request --header \"Content-Type: application/json\" -X POST -d '{"my-secret":"bar"}' \ PUT -d {\"my-value\",\"" + vaultToken + "\"} " + vaultAddress + "/v1/kv/my-secret | jq"
         //sh "curl --header \"X-Vault-Token: " + vaultToken + "\" --request POST -d {\"my-secret\",\"" + vaultToken + "\"} " + vaultAddress + "/v1/kv/my-secret | jq"
         println "curl --header \"X-Vault-Token: " + vaultToken + "\" --request GET " + vaultAddress + "/v1/kv/my-secret | jq"
         sh "curl --header \"X-Vault-Token: " + vaultToken + "\" --request GET " + vaultAddress + "/v1/kv/my-secret | jq"    
