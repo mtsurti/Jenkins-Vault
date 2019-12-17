793b0cea8653ab09afc098b0186860ed07c87be4
mtsurti <tahir.clinar@gmail.com>
2019-12-17 14:26:40 -0600

    Update pushVaultToken.groovy

--- pushVaultToken.groovy
+++ pushVaultToken.groovy
@@ -42,7 +42,7 @@ import hudson.util.Secret
        vaultToken != null && vaultToken.length() > 0 &&
        vaultAddress != null && vaultAddress.length() > 0) {
         println "vault write -address="+ vaultAddress + "v1/kv/my-secret value=" + vaultToken
-        sh "vault write -address="+ vaultAddress + "v1/kv/my-secret value=" + vaultToken + "\""
+        sh "vault write -address="+ vaultAddress + "v1/kv/my-secret value=" + vaultToken
 
         //println "curl --header \"X-Vault-Token: " + vaultToken + "\" --request PUT -d {\"my-value\",\"" + vaultToken + "\"} " + vaultAddress + "/v1/kv/my-secret | jq"
         //sh "curl --header \"X-Vault-Token: " + vaultToken + "\" --request POST -d {\"my-secret\",\"" + vaultToken + "\"} " + vaultAddress + "/v1/kv/my-secret | jq"
