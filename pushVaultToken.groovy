33101b6b58b2dd23de27f70e99ed73596853d3e3
mtsurti <tahir.clinar@gmail.com>
2019-12-19 19:01:33 -0600

    Update pushVaultToken.groovy

--- pushVaultToken.groovy
+++ pushVaultToken.groovy
@@ -8,8 +8,8 @@ def updateVaultToken(String authToken, String vaultToken, String vaultAddress) {
     if (authToken != null && authToken.length() > 0 &&
        vaultToken != null && vaultToken.length() > 0 &&
        vaultAddress != null && vaultAddress.length() > 0) { 
-        sh "curl --header \'X-Vault-Token: ${vaultToken}\' --header \'Content-Type: application/json\' -X POST -d \'{\"auth-token\":\"${authToken}\"}\' ${vaultAddress}/v1/kv/my-secret"
-        sh "curl --header \'X-Vault-Token: ${vaultToken}\' --request GET ${vaultAddress}/v1/kv/my-secret | jq"    
+        sh "curl --header \"X-Vault-Token\": \"${vaultToken}\"\' --header \'Content-Type: application/json\' -X POST -d \'{\"auth-token\":\"${authToken}\"}\' ${vaultAddress}/v1/kv/my-secret"
+        sh "curl --header \"X-Vault-Token\": \"${vaultToken}\"\' --request GET ${vaultAddress}/v1/kv/my-secret | jq"    
     }
   }
 return this
