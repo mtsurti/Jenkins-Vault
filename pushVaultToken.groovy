1b0c37d2e5d8cd0143243229b00fee25f6835c57
mtsurti <tahir.clinar@gmail.com>
2019-12-24 12:43:29 -0600

    Update pushVaultToken.groovy

--- pushVaultToken.groovy
+++ pushVaultToken.groovy
@@ -10,18 +10,21 @@ def updateVaultToken(String authToken, String vaultToken, String vaultAddress) {
        vaultAddress != null && vaultAddress.length() > 0) { 
         //sh "curl --header \'X-Vault-Token: \"${vaultToken}\"\' --header \'X-Vault-Namespace: authtoken/\' --header \'Content-Type: application/json\' -X POST -d \'{\"auth-token\":\"${authToken}\"}\' ${vaultAddress}/v1/kv/my-secret"
         //sh "curl --header \'X-Vault-Token: \"${vaultToken}\"\' --request GET ${vaultAddress}/v1/authtoken/my-secret | jq"    
+
         def secrets = [
         [path: 'kv/vaulttoken', secretValues: [
-            [vaultKey: 'authtoken']]]
-    ]
+            [envVar: 'authtoken', vaultKey: \"${authToken}\"]],
+        [path: 'kv/vaulttoken', secretValues: [
+            [vaultKey: \"${authToken\"}]]
+        ]    
  
     // optional configuration, if you do not provide this the next higher configuration
     // (e.g. folder or global) will be used
         def configuration = [vaultUrl: 'http://127.0.0.1:9200',
-                         vaultCredentialId: 'auth-writer']
+                         vaultCredentialId: 'auth-reader']
     // inside this block your credentials will be available as env variables
     withVault([configuration: configuration, vaultSecrets: secrets]) {
-        sh "echo $authtoken"
+        sh 'echo $authtoken'
     }
     }
   }
